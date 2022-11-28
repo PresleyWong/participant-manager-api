e4cd51ce726eab88a60ed520f6239d8a9288f1c0
PresleyWong <presley_wg@yahoo.com>
2022-11-28 13:22:13 +0800

    initial commit

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,16 @@
+class AuthenticationController < ApplicationController
+    skip_before_action :authenticate_user
+
+    def login
+        @user = User.find_by_email(params[:email])
+        if @user&.authenticate(params[:password])
+            token = jwt_encode(user_id: @user.id)
+            time = Time.now + 24.hours.to_i
+            render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"), 
+                            email: @user.email }, status: :ok
+        else
+            render json: { errors: 'unauthorized' }, status: :unauthorized
+        end
+    end
+
+end
